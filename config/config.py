98515a070a6d9015d7404b4ffb16920d5a1ee706
AndersonAsto <axelasto10@gmail.com>
2025-09-28 11:25:37 -0500

    v0.10-28/09/25

--- config/config.py
+++ config/config.py
@@ -2,18 +2,18 @@ import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
-load_dotenv()
+load_dotenv()  # lee .env si existe
 db = SQLAlchemy()
 
 class Config:
-    MYSQL_USER = os.getenv("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-    MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-    MYSQL_DB = os.getenv("MYSQL_DB", "sistema_tambo")
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = os.getenv("MYSQL_PORT")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?charset=utf8mb4&timezone=America/Lima"
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?charset=utf8mb4"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
